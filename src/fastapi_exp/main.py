07c2ed5678766e2680b3ca87a3322959a848de29
Ray Doue <rrdoue@gmail.com>
2025-10-12 09:57:40 -0500

    Working on some progress regarding the project, the additions are all placeholders for actual functionality.

--- src/fastapi_exp/main.py
+++ src/fastapi_exp/main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -24,3 +25,14 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item_name, "item_id": item_id}
+
+
+@app.get('/data/export/hr_sample/all')
+def export_all_hr_sample_data():
+    return {'message': 'Placeholder for hr_sample database export.'}
+
+
+@app.get('/data/export/hr_sample/all/changes')
+def export_all_hr_sample_data():  # from_date: datetime, to_date: datetime
+    return {'message': 'Placeholder for hr_sample database export using time-based query parameters.  If a to_date is '
+                       'not included, the current date defaults to current date 0000 UTC.'} 
